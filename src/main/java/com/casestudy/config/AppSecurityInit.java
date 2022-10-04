b40804e45f85381dc6dd9ca54bafd6d4cf06a1d1
ishopjapan <thoitran2107@gmail.com>
2022-10-04 13:47:18 +0700

    tao iservice

--- src/main/java/com/casestudy/config/AppSecurityInit.java
+++ src/main/java/com/casestudy/config/AppSecurityInit.java
@@ -3,5 +3,4 @@ package com.casestudy.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class AppSecurityInit extends AbstractSecurityWebApplicationInitializer {
-    adfgadfl
 }
