f578fe42bb62f9f05bebce8c3da263327fb3e3b9
Hiep Nguyen <gagga5376@gmail.com>
2022-10-04 13:56:40 +0700

    viet bill repo

---
+++ src/main/java/com/casestudy/repository/BillRepository.java
@@ -0,0 +1,16 @@
+package com.casestudy.repository;
+
+import com.casestudy.model.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill,Long> {
+    @Query(value = "select * from bill where login_user_id = ? and status = false;", nativeQuery = true)
+    public List<Bill> findBillNotPayByUserId(Long userId);
+
+
+}
