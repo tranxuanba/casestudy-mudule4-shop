abb94ddad430bb81765e9f8260202083079cfb4a
MinhHoang3011 <minhhoang301198@gmail.com>
2022-10-04 13:54:02 +0700

    update repository

---
+++ src/main/java/com/casestudy/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.casestudy.repository;
+
+import com.casestudy.model.Product;
+import com.casestudy.model.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+    Page<Product> getAllByShop(Shop shop, Pageable pageable);
+
+    @Query(value = "select * from product", nativeQuery = true)
+    List<Product> showAllProduct();
+
+    //tim kiem theo ten
+    @Query(value = "select * from product where product.name like ?", nativeQuery = true)
+    List<Product> findProductByName(String name);
+}
