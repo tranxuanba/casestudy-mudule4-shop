e67836942fb9614a168cb5846557a0c43d01799c
MinhHoang3011 <minhhoang301198@gmail.com>
2022-10-05 08:40:25 +0700

    fix git

--- src/main/java/com/casestudy/config/AppSecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.casestudy.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class AppSecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
