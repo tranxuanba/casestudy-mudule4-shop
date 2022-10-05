36313961142c8eec2632640fa91cbd5e342ba5f8
Hiep Nguyen <gagga5376@gmail.com>
2022-10-05 13:51:04 +0700

    conflict

--- src/main/java/com/casestudy/service/billDetail/IBillDetailService.java
+++ src/main/java/com/casestudy/service/billDetail/IBillDetailService.java
@@ -7,12 +7,6 @@ import com.casestudy.service.IService;
 import java.util.List;
 
 public interface IBillDetailService extends IService<BillDetail> {
-<<<<<<< HEAD
-
-
     List<BillDetail> findALlByBill(Bill bill);
-=======
-    List<BillDetail> findAllByBill(Bill bill);
->>>>>>> 626900eee150dd74d2c04f35d27a8282433dbe59
     Double calculateMoneyByBillId(Long BillId);
 }
