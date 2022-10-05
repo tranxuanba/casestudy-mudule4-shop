4927c1a8df514cc0bd7c708b4b6c6e61d95d4b79
ishopjapan <thoitran2107@gmail.com>
2022-10-05 09:32:42 +0700

    update repo

---
+++ src/main/java/com/casestudy/repository/StatisticalRepository.java
@@ -0,0 +1,14 @@
+package com.casestudy.repository;
+
+
+import com.casestudy.model.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatisticalRepository extends JpaRepository<Bill, Long> {
+    @Query(value = "SELECT SUM(total_money) as 'total' FROM bill WHERE MONTH(date) = :month AND shop_id = :shopId ", nativeQuery = true)
+    Long getTotal(@Param("month") Integer month, @Param("shopId") Long shopId);
+}
